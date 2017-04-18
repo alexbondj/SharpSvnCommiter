796890a0d9d251f869d91dbb474e21eadf476792
alexbondj <alexbondj@gmail.com>
2017-04-18 20:53:15 +0300

    Load Project

---
+++ SharpSvnCommiter/Components/SvnUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpSvn;
+
+namespace SharpSvnCommiter.Components
+{
+	public class SvnUtil
+	{
+		private IList<string> GetLatestCommitMessages(Uri repository, int count = 10) {
+			using (var client = new SvnClient()) {
+				System.Collections.ObjectModel.Collection<SvnLogEventArgs> logEntries;
+				var args = new SvnLogArgs() {
+					Limit = count
+				};
+				client.GetLog(repository, args, out logEntries);
+				return logEntries.Select(log => log.LogMessage).ToList();
+			}
+		}
+
+		public IList<string> GetLogMessages(Uri repository) {
+			return GetLatestCommitMessages(repository);
+		}
+	}
+}
---
+++ SharpSvnCommiter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpSvnCommiter.Components;
+
+namespace SharpSvnCommiter
+{
+	class Program
+	{
+		static void Main(string[] args) {
+			var svnUtil = new SvnUtil();
+			var messages = svnUtil.GetLogMessages(new Uri(@"http://tscore-svn:8050/svn/ts5conf/PackageStore"));
+			Console.WriteLine(messages);
+			Console.ReadKey();
+		}
+	}
+}
