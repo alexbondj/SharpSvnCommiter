ab91313fe184ed779a6ab87b05b12fc9752823dc
alexbondj <alexbondj@gmail.com>
2017-04-19 21:04:00 +0300

    Add Entities folder

--- SharpSvnCommiter/Components/SvnUtil.cs
+++ SharpSvnCommiter/Components/SvnUtil.cs
@@ -9,22 +9,30 @@ namespace SharpSvnCommiter.Components
 {
 	public class SvnUtil
 	{
-		private IList<string> GetLatestCommitMessages(Uri repository, int start) {
+
+		public SvnUtil(DateTime start, DateTime end)
+		{
+			Start = start;
+			End = end;
+		}
+
+		public DateTime Start { get; private set; }
+
+		public DateTime End { get; private set; }
+
+		private IList<string> GetLatestCommitMessages(Uri repository) {
 			using (var client = new SvnClient()) {
 				System.Collections.ObjectModel.Collection<SvnLogEventArgs> logEntries;
-				//var args = new SvnLogArgs() {
-				//	Start = start
-				//};
-				DateTime startDateTime = new DateTime(2017, 03, 20);
-				DateTime endDateTime = DateTime.Now;
-				SvnRevisionRange range = new SvnRevisionRange(new SvnRevision(startDateTime), new SvnRevision(endDateTime));
-				client.GetLog(repository, new SvnLogArgs(range), out logEntries);
+				
+				SvnRevisionRange range = new SvnRevisionRange(new SvnRevision(Start), new SvnRevision(End));
+				var args = new SvnLogArgs(range);
+				client.GetLog(repository, args, out logEntries);
 				return logEntries.Where(log => log.LogMessage.Contains("#SD-3606")).Select(log => log.LogMessage).ToList();
 			}
 		}
 
 		public IList<string> GetLogMessages(Uri repository) {
-			return GetLatestCommitMessages(repository, 120640);
+			return GetLatestCommitMessages(repository);
 		}
 	}
 }
--- SharpSvnCommiter/Program.cs
+++ SharpSvnCommiter/Program.cs
@@ -10,7 +10,9 @@ namespace SharpSvnCommiter
 	class Program
 	{
 		static void Main(string[] args) {
-			var svnUtil = new SvnUtil();
+			DateTime startDateTime = new DateTime(2017, 03, 20);
+			DateTime endDateTime = DateTime.Now;
+			var svnUtil = new SvnUtil(startDateTime, endDateTime);
 			var messages = svnUtil.GetLogMessages(new Uri(@"http://tscore-svn:8050/svn/ts5conf/PackageStore"));
 			Console.WriteLine(messages);
 			Console.ReadKey();
